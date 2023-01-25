ab097d6f5c7ff4e40cb7fb28d3de67c52cb37662
Kagiso Modika <kagisomodika@yahoo.com>
2023-01-25 20:50:36 +0200

    Added a method to change the background of items.

---
+++ toggleEvenColour.js
@@ -0,0 +1,6 @@
+function toggleEvenColor() {
+    let spanElements = document.querySelectorAll('#numbers span:nth-child(2n)');
+    spanElements.forEach(function(item) {
+        item.style.backgroundColor = item.style.backgroundColor !== 'yellow' ? 'yellow' : 'transparent';
+    });
+}
